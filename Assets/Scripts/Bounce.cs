bf910de65636970a93b18dd037b470eb6e330509
Shayla L <lees846@newschool.edu>
2022-03-09 10:22:58 -0500

    Alternative code to bounce script

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 public class Bounce : MonoBehaviour
 {
 
-    // public Vector3 transformation = new Vector3(0, 1, 0);
+    // public fields show up in the unity editor
     public float amplitude = 2f;
     public float frequency = 0.03f;
     public float offset = 0f;
 
+    public float speed;
+    public float height;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Vector3 direction = new Vector3(0, 1, 0);
+        // transform.Translate(direction);
+
+        // sets position exactly
+        // transform.position = direction;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = new Vector3(
-            transform.localPosition.x,
-            amplitude * Mathf.Sin(Time.frameCount * frequency + offset) + 2f,
-            transform.localPosition.z
-        );
+        // transform.localPosition = new Vector3(
+        //     transform.localPosition.x,
+        //     amplitude * Mathf.Sin(Time.frameCount * frequency + offset) + 2f,
+        //     transform.localPosition.z
+        // );
+
+        Vector3 newPosition = new Vector3(transform.position.x,
+        height * Mathf.Sin(speed * Time.frameCount + offset) + 5,
+        transform.position.z);
+        transform.position = newPosition;
     }
 }
