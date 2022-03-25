9eeedb29eb171db55687f870bca5028993d62f89
Shayla L <lees846@newschool.edu>
2022-03-25 11:37:32 -0400

    Added first person scene with spinning door on trigger

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    AudioSource myAudioSource;
+
+    // public Vector3 door
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")){
+            myAudioSource.Play();
+
+            // when triggered, rotate the door from the hinge
+            transform.Rotate(0, -90, 0);
+
+        }
+    }
+
+}
